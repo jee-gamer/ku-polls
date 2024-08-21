97150c2237a0fc0afc11f23d4d4179cfed174eb1
Jiramate Kedmake <jiramate2548@gmail.com>
2024-08-21 18:12:49 +0700

    Tutorial part 5 Testing done

--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,36 @@
-from django.shortcuts import render, get_object_or_404
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question, Choice
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions"""
+        return Question.objects.filter(pub_date__lte=timezone.now())\
+                   .order_by("-pub_date")[:5]
 
 
 
-def index(request):
-    lastest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"lastest_question_list": lastest_question_list}
-    return render(request, "polls/index.html", context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """Exclude any question that aren't published yet."""
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
@@ -33,7 +44,7 @@ def vote(request, question_id):
             "polls/detail.html",
             {
                 "question": question,
-                "error_message": "You didn't select a choice."
+                "error_message": "You didn't select a choice.",
             },
         )
     else:
@@ -42,5 +53,5 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-
-        return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
+        return HttpResponseRedirect(
+            reverse("polls:results", args=(question.id,)))
